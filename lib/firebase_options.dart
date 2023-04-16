650360a97f6f30eeb4bcfec45412df75c584f181
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-04-16 10:42:01 +0600

    Version.1

---
+++ lib/firebase_options.dart
@@ -0,0 +1,15 @@
+// TODO Implement this library.
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+class ProFirebaseOptions {
+  static const FirebaseOptions current = FirebaseOptions(
+      apiKey: "AIzaSyBw5g7o3rer7XfxgX7EzbKzNTcF5eR9Qqw",
+      appId: "1:974543933024:android:4c83edf67f15770b943d05",
+      messagingSenderId: '974543933024',
+      projectId: 'project-974543933024',
+      databaseURL:
+      'https://praktika7-54b11-default-rtdb.firebaseio.com',
+      storageBucket: 'praktika7-54b11.appspot.com',
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: ProFirebaseOptions.current,
+  );
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Control(),
+    );
+  }
+}
+
+class Control extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return ControlState();
+  }
+}
+
+
+
+
+class ControlState extends State<Control> {
+  final databaseReference = FirebaseDatabase.instance.ref();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            databaseReference.child('message').set('Salem Alem!');
+          },
+          child: Text("Jiberu"),
+        ),
+      ),
+    );
+  }
+}
